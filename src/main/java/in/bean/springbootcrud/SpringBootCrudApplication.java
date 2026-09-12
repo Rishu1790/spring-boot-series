73c3f76e732be6be3903cd4485fef19c2f866e20
Rishu Ranjan 17 <luvkush9534901790@gmail.com>
2026-09-13 00:34:31 +0530

    Spring cHAnges

---
+++ src/main/java/in/bean/springbootcrud/SpringBootCrudApplication.java
@@ -0,0 +1,16 @@
+package in.bean.springbootcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class SpringBootCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCrudApplication.class, args);
+        System.out.println("SpringBoot");
+    }
+
+}
